eeddcfeb83ccf44de3917fcefddc49fbb54242c8
gopi-006 <you@example.com>
2024-07-30 18:39:40 +0530

    developed file

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsComponent} from "./../forms/forms.component"
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsComponent,ParentComponent,ChildComponent,HomeComponent,NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = ' bhtnhujthtn';
 }
--- my-first-project/src/app/app.routes.ts
+++ my-first-project/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FormsComponent } from '../forms/forms.component';
+import { ParentComponent } from './parent/parent.component';
+import { HomeComponent } from './home/home.component';
+import { ChildComponent } from './child/child.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+     title:'',
+     component:HomeComponent,
+    },
+    {
+        path:'home',
+     title:'',
+     component:HomeComponent,
+    },
+    {
+        path:'forms',
+     title:'',
+     component:FormsComponent,
+    },
+    {
+        path:'parent',
+     title:'',
+     component:ParentComponent,
+    },
+   
+    
+];
